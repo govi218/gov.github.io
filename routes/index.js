3c310ecce20a1ea359d7317a4af0711d111ce8a3
Govind Mohan <govi218mu@gmail.com>
2017-08-08 22:34:18 -0400

    added hyperlinks

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Govind Mohan'
   , github: 'https://github.com/gov218'
-  , email: 'info@govindmohan.com'});
+  , email: 'info@govindmohan.com',
+    contact: '+1 (647) 261-7358'});
 });
 
 module.exports = router;
